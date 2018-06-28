71311fe1c84a009452d104b9e0f269f9c994bee7
Nate Rudnick-Aldridge <nate@curagoapp.com>
2018-06-27 23:51:42 -0500

    Continuing rebuild

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,7 +1,6 @@
 class EpisodesController < ApplicationController
   def index
-    # @episodes = Episode.all.order('episodes.number DESC')
-    @episodes = Episode.last(10)
+    @episodes = Episode.order(number: :desc).first(5)
     @top5 = Timestamp.order(cached_votes_total: :desc).first(5)
     @randoms = Timestamp.order("RANDOM()").limit(5)
   end
